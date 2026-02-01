93d316d0a9953deb001577046a558ac25ffe8039
kali <kali@kali.kali>
2026-02-01 19:29:46 +0100

    Mise à jour

--- target-jenkins/entrypoint.sh
+++ target-jenkins/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# 1. Démarrer l'agent Wazuh en arrière-plan
+# 1. Démarrage de Wazuh (Doit être fait en ROOT)
+echo "🔴 Démarrage de l'agent Wazuh..."
 service wazuh-agent start
 
-# 2. Démarrer Jenkins (commande officielle de l'image)
-# On utilise le script de démarrage standard de Jenkins
-/usr/local/bin/jenkins.sh
+# 2. Lancement de Jenkins (On passe la main à l'utilisateur 'jenkins')
+# 'su jenkins -c' exécute la commande en tant que cet utilisateur
+echo "🟢 Lancement de Jenkins (User: jenkins)..."
+su jenkins -c "/usr/bin/tini -- /usr/local/bin/jenkins.sh"
